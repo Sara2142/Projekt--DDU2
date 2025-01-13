27342ee11e6ae5b286449eec0b74520049ea1f41
Sara <so013872@gmail.com>
2025-01-13 19:20:16 +0100

    funktion som ger ett random nummer via ett anrop. Jag anropar med numrena 0 och 99 eftersom det är det intervallet jag ska ha

--- common.js
+++ common.js
@@ -1,2 +1,5 @@
 const mainDOM = document.querySelector("main");
 const cellParent = document.getElementById("cellParent")
+function randomInt(min, max) {
+    return min + Math.floor((max+1- min) * Math.random());
+ }
