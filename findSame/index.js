cf325537516e841fe549669c4e8704e1f5fc8d6b
Sara <so013872@gmail.com>
2025-01-13 21:28:25 +0100

    klickar på en cell och sparar siffran sen loopar vi igenom alla celler om dem har samma siffra läggs en klass till och countern går upp för varje gång det matchar. Sedan variablerna för information

--- findSame/index.js
+++ findSame/index.js
@@ -1,1 +1,17 @@
 function findSame(){
+    for(let cell of cells){
+    let cellClicked = ""
+    let counter = 0
+        cell.addEventListener("click", function(){
+             cellClicked = cell.textContent
+             for (let cell of cells){
+                if (cell.textContent == cellClicked){
+                    cell.classList.add("found")
+                    counter++
+                } else{
+                    cell.classList.remove("found")
+                }
+                textAboutButton.textContent = `${counter} copies of the number ${cellClicked}`
+             }
+        })
+    
