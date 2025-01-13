5ae06c54da5c70bef8323921b6ad9978da10dfcb
Sara <so013872@gmail.com>
2025-01-13 20:55:01 +0100

    funktion som adderar värdet av alla celler tillsammans. Summan läggs  i första input

--- sum/index.js
+++ sum/index.js
@@ -13,3 +13,12 @@ text2Input.value = "-"
 const resetButton = document.createElement("button")
 sumOfMarkedDOM.appendChild(resetButton)
 resetButton.textContent = "Reset"
+function sumofAllCells(){
+    const cells = document.querySelectorAll(".cell")
+    let total = 0
+    for (let cell of cells){
+        total += Number(cell.textContent)
+    }
+    return total;
+}
+text1Input.value = sumofAllCells()
