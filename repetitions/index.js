fab0d010d5ba47015580bfbea53ff99c32d771db
Sara <so013872@gmail.com>
2025-01-13 21:05:51 +0100

    loopar igenom alla siffror mellan 0 - 99 om cellen inte har det då pushas den siffran som inte finns i en array.
    Arrayen blir till innehållet av div elementet

--- repetitions/index.js
+++ repetitions/index.js
@@ -2,3 +2,23 @@ const notInPlaceText = document.createElement("div")
 notInPlaceDOM.appendChild(notInPlaceText)
 notInPlaceText.id= "notInPlaceText"
 const cells = document.querySelectorAll(".cell")
+function findNotInPlaceNumbers(){
+    const cells = document.querySelectorAll(".cell")
+    let notInPlaceNumbers = []
+
+
+    for (let i = 0; i <= 99; i++) {
+        let found = false; 
+        for (let cell of cells) {
+            if (Number(cell.textContent) === i) {
+                found = true; 
+            }
+        }
+        if (found === false) {
+            notInPlaceNumbers.push(i); 
+        }
+    }
+    notInPlaceText.textContent = notInPlaceNumbers.join(", ")
+}
+
+findNotInPlaceNumbers()
