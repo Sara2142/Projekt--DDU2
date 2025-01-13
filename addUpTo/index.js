af6ea8a85805fca647bb478d46d6def99888542a
Sara <so013872@gmail.com>
2025-01-13 21:34:05 +0100

    vi använder nästlaste loopar för att kunna spara två variabler och sedan se om dem tillsammans har sammma summa som input  och då läggs klasser som ändrar utseendet.

--- addUpTo/index.js
+++ addUpTo/index.js
@@ -1,1 +1,22 @@
 function findTwoCells() {
+    let cells = document.querySelectorAll(".cell");
+
+    for (let i = 0; i < cells.length; i++) {
+        for (let j = 0; j < cells.length; j++) {
+            let cell1 = Number(cells[i].textContent);
+            let cell2 = Number(cells[j].textContent);
+
+
+            if (cell1 + cell2 === Number(inputForAddUp.value)) {
+
+                cells[i].classList.add("found");
+                cells[j].classList.add("found");
+                return;  
+            } else {
+                cells[i].classList.remove("found");
+                cells[j].classList.remove("found");
+            }
+        }
+    }
+
+}
