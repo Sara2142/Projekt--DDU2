4322293c28627927bf6c0ea142e9e2d2b8db6dbd
Sara <so013872@gmail.com>
2025-01-13 20:25:18 +0100

    skapar alla knappar och input element sen appendar jag dem till sidans html element

---
+++ remove/index.js
@@ -0,0 +1,21 @@
+const twoButtons = document.getElementById("twoButtonsArea")
+
+const randomNumButton = document.createElement("button")
+twoButtons.appendChild(randomNumButton)
+randomNumButton.textContent = "New Random Number"
+randomNumButton.id = "randomNumButton"
+
+const randomNumInput = document.createElement("input")
+twoButtons.appendChild(randomNumInput)
+randomNumInput.value = "-"
+randomNumInput.id = "randomNumInput"
+
+const removeButton = document.createElement("button")
+twoButtons.appendChild(removeButton)
+removeButton.textContent = "Remove"
+removeButton.id = "removeButton"
+
+const removeNumInput = document.createElement("input")
+twoButtons.appendChild(removeNumInput)
+removeNumInput.value = "-"
+removeNumInput.id = "removeNumInput"
